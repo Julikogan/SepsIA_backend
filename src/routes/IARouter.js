5d70b7a489e76b3c6e3eb7c89541781a91f4b772
Julikogan <48243253@est.ort.edu.ar>
2025-08-20 14:11:09 -0300

    borre archivos que no servian
    ya esta la conexion con la ia

--- src/routes/IARouter.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { analizarSignos } from "../controllers/IAController.js";
-
-const router = Router();
-
-// POST /ia/analizar/:pacienteId
-router.post("/analizar/:pacienteId", analizarSignos);
-
-export default router;
